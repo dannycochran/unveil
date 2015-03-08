0862998d10aa6e206e075e895fe2b751a11ab108
dannycochran <daniel@cochrans.org>
2015-03-08 15:09:33 -0700

    my fork of unveil

--- README.md
+++ README.md
@@ -1,19 +1,45 @@
 #unveil.js
 ###A very lightweight plugin to lazy load images for jQuery or Zepto.js
 
+A fork of [Luis Almeida's unveil](http://luis-almeida.github.com/unveil/).
 
 
-Most of us are familiar with the [Lazy Load](http://www.appelsiini.net/projects/lazyload) plugin by [Mika Tuupola](http://www.appelsiini.net/).
-This plugin is very useful and it boosts performance delaying loading of images in long web pages because images outside of viewport (visible part of web page) won't be loaded until the user scrolls to them.
-Lazy Load has some cool options such as custom effects, container, events or data attribute. If you're not gonna use any of them you can reduce the file size by leaving just the essential code to show the images.
-That's what I did and this is my lightweight version of Lazy Load with support for serving high-resolution images to devices with retina displays - less than 1k.
+My own version of unveil that allows for more fine tuned control of the image unveiler (you can specifiy a container and start & stop the scroll/resize listeners).
 
-Visit unveil's [project page](http://luis-almeida.github.com/unveil/) to read the documentation and see the demo.
+### Example Usage
+
+When rendering a view:
 
 
-###Browser support
-Compatible with All Browsers and IE7+.
+```
+var $container = this.$('ol.container'),
+    callback = function () {
+      var $el = $(this);
+      $el.addClass('loading');
+      $el.one('load', function () {
+        setTimeout(function () { $el.removeClass('loading'); }, 1000);
+      });
+    };
 
+// instaniate unveiler with 0 threshold, a callback for applying an opacity transition, and the $container to watch for scroll events
+var unveiler = $container.find('img').unveil(0, callback, $container);
+
+// if necessary, set all your images 'src' attributes to be 'data-src' instead
+// this is useful if you're pulling from an API (like tumblr) and can't control the HTML template
+unveil.replaceDataSrc()
+
+// start listening for scroll and resize events
+unveiler.start();
+
+// do an initial unveil
+unveiler.unveil();
+```
+
+When destroying the view or moving to a new view:
+
+```
+unveiler.stop();
+```
 
 ###License
 Unveil is licensed under the [MIT license](http://opensource.org/licenses/MIT).
--- jquery.unveil.js
+++ jquery.unveil.js
@@ -9,48 +9,65 @@
  */
 
 ;(function($) {
-
-  $.fn.unveil = function(threshold, callback) {
-
+  $.fn.unveil = function(threshold, callback, $target) {
     var $w = $(window),
+        $container = $target || $w,
         th = threshold || 0,
         retina = window.devicePixelRatio > 1,
-        attrib = retina? "data-src-retina" : "data-src",
+        attrib = retina? 'data-src-retina' : 'data-src',
         images = this,
-        loaded;
+        loading = '';
 
-    this.one("unveil", function() {
+    function onUnveil() {
       var source = this.getAttribute(attrib);
-      source = source || this.getAttribute("data-src");
+      source = source || this.getAttribute('data-src');
       if (source) {
-        this.setAttribute("src", source);
-        if (typeof callback === "function") callback.call(this);
+        this.removeAttribute('data-src');
+        this.setAttribute('src', source);
+        if (typeof callback === 'function') callback.call(this);
       }
-    });
+    }
 
     function unveil() {
       var inview = images.filter(function() {
-        var $e = $(this);
-        if ($e.is(":hidden")) return;
-
-        var wt = $w.scrollTop(),
-            wb = wt + $w.height(),
-            et = $e.offset().top,
-            eb = et + $e.height();
-
-        return eb >= wt - th && et <= wb + th;
+        var $img = $(this);
+        if ($img.css('display') === 'none') return;
+        else {
+          var imageTop = $img.offset().top,
+              imageBottom = imageTop + $img.height(),
+              containerTop = $container.offset().top,
+              containerBottom = $container.height();
+          return imageTop >= containerTop - th && imageBottom <= containerBottom + th;
+        }
       });
-
-      loaded = inview.trigger("unveil");
-      images = images.not(loaded);
+      // load just one image at a time and then check for more after load is finished
+      var $first = $(inview[0]);
+      $first.trigger('unveil');
+      $first.one('load', function () { unveil(); });
+      images = images.not($first);
     }
 
-    $w.on("scroll.unveil resize.unveil lookup.unveil", unveil);
+    return {
+      replaceDataSrc: function () {
+        images.each(function () {
+          var src = this.getAttribute('src');
+          this.setAttribute('src', loading);
+          this.setAttribute(attrib, src);
+        });
 
-    unveil();
-
-    return this;
-
+        return this;
+      },
+      unveil: function () { unveil(); },
+      start: function () {
+        images.one('unveil', onUnveil);
+        $container.on('scroll', unveil);
+        $w.resize(unveil);
+      },
+      stop: function ($target) {
+        images.off('unveil', onUnveil);
+        $container.off('scroll', unveil);
+        $w.off('resize', unveil);
+      }
+    };
   };
-
 })(window.jQuery || window.Zepto);
